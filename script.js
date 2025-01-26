5ebf74f6b58446387bc88f3c83fdf4ef982d46eb
Adam.L <adkheydiscord@gmail.com>
2025-01-26 20:58:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Calculator Program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function Clear() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        
+        display.value = eval(display.value);
+        } catch (error) {
+            display.value = "Error";
+        }
+
+}
